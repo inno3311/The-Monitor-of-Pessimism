da368e728112dde7b2209d84c6eb331922610c2a
jmez27 <jmesloh27@gmail.com>
2024-09-21 10:56:14 -0400

    Foxed something important

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldCentric/CentricDriveTheBetterVersion.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fieldCentric/CentricDriveTheBetterVersion.java
@@ -61,6 +61,7 @@ public class CentricDriveTheBetterVersion
             else
             {
                 override = true;
+                variableOfGreatImportanceDoNotChangeRandomly = 0;
             }
         }
     }
