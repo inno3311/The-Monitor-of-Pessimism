ea95695b7431fc05f4562ce5a41acabc06739b77
InnovatorsRobotics <innovators3311@gmail.com>
2024-10-22 19:38:47 -0400

    Encoder presets done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototype/ProtoLinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototype/ProtoLinearSlide.java
@@ -6,9 +6,10 @@ import org.firstinspires.ftc.teamcode.controller.MotorControl;
 
 public class ProtoLinearSlide extends MotorControl
 {
+
     public ProtoLinearSlide(LinearOpMode opMode)
     {
-        super("linearSlide", true, false, opMode);
+        super("linearSlide", true, true, opMode);
     }
 
     @Override
@@ -17,4 +18,46 @@ public class ProtoLinearSlide extends MotorControl
         super.analogControl(speedLimit, input, advanceBreak);
     }
 
+    public enum Presets
+    {
+        TOP_CHAMBER,
+        BOTTOM_CHAMBER,
+        TOP_BUCKET,
+        BOTTOM_BUCKET,
+        PICKUP_FLOOR,
+        PICKUP_WALL
+    }
+
+    public void encoderPresets(Presets preset)
+    {
+        switch (preset)
+        {
+            case TOP_CHAMBER:
+                super.encoderControl(-920,1);
+                break;
+            case BOTTOM_CHAMBER:
+                super.encoderControl(-375,1);
+                break;
+            case TOP_BUCKET:
+                super.encoderControl(-2270,1);
+                break;
+            case BOTTOM_BUCKET:
+                super.encoderControl(-1 * 920,1);
+                break;
+            case PICKUP_FLOOR:
+                super.encoderControl(0,1);
+                break;
+            case PICKUP_WALL:
+                super.encoderControl(1,1);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void telemetry()
+    {
+        super.telemetry();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototype/ProtoMaster.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prototype/ProtoMaster.java
@@ -21,7 +21,7 @@ public class ProtoMaster extends LinearOpMode
     // Accessories
     ProtoLinearSlide linearSlide;
     ProtoSlideTheta slideTheta;
-    ProtoHang protoHang;
+    ProtoHang hang;
     ProtoWrist clawWrist;
     ProtoClaw claw;
 
@@ -34,7 +34,7 @@ public class ProtoMaster extends LinearOpMode
         centricDrive = new CentricDrive(mechanicalDriveBase, telemetry);
         linearSlide = new ProtoLinearSlide(this);
         slideTheta = new ProtoSlideTheta(this);
-        protoHang = new ProtoHang(this);
+        hang = new ProtoHang(this);
         clawWrist = new ProtoWrist(this);
         claw = new ProtoClaw(this);
 
@@ -42,15 +42,38 @@ public class ProtoMaster extends LinearOpMode
 
         while (opModeIsActive())
         {
+
             //        centricDrive.drive(gamepad1.left_stick_x, gamepad1.left_stick_y, imu.getAngle(), turnToHeading.turnToHeading(gamepad1.right_stick_x, gamepad1.right_stick_y, 0.2, 0.2));
 
             mechanicalDriveBase.gamepadController(gamepad1);
 
+            if (gamepad2.dpad_up)
+            {
+                linearSlide.encoderPresets(ProtoLinearSlide.Presets.TOP_CHAMBER);
+                slideTheta.encoderPresets(ProtoSlideTheta.Presets.TOP_CHAMBER);
+            }
+            else if (gamepad2.dpad_down)
+            {
+                linearSlide.encoderPresets(ProtoLinearSlide.Presets.PICKUP_FLOOR);
+                slideTheta.encoderPresets(ProtoSlideTheta.Presets.PICKUP_FLOOR);
+            }
+            else if (gamepad2.dpad_left)
+            {
+                linearSlide.encoderPresets(ProtoLinearSlide.Presets.TOP_BUCKET);
+                slideTheta.encoderPresets(ProtoSlideTheta.Presets.TOP_BUCKET);
+            }
+            else if (gamepad2.dpad_right)
+            {
+                linearSlide.encoderPresets(ProtoLinearSlide.Presets.BOTTOM_BUCKET);
+                slideTheta.encoderPresets(ProtoSlideTheta.Presets.BOTTOM_BUCKET);
+            }
+            else
+            {
             linearSlide.analogControl(0.5, gamepad2.left_stick_y, true);
-
             slideTheta.analogControl(1, gamepad2.right_stick_y, false);
+            }
 
-            protoHang.simpleDrive(1, gamepad2.y, gamepad2.a);
+            hang.simpleDrive(1, gamepad2.y, gamepad2.a);
 
             if (gamepad2.right_bumper)
             {
@@ -69,6 +92,11 @@ public class ProtoMaster extends LinearOpMode
             {
                 clawWrist.driveServo(0.5);
             }
+
+            linearSlide.telemetry();
+            slideTheta.telemetry();
+            hang.telemetry();
+            telemetry.update();
         }
 
     }
