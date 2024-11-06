52770f79961f64208075c184f8346386176c6975
jmez27 <jmesloh27@gmail.com>
2024-11-05 19:41:25 -0500

    WIP! Second level chamber hang
    STILL A WORK IN PROGRESS!!! The arm now consistently places sample onto the second level of the chamber. Just a save point to revert back to when needed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/HighChamberSpecimens.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/HighChamberSpecimens.java
@@ -8,6 +8,9 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.teamcode.initialization.Initialization;
 import org.firstinspires.ftc.teamcode.prototype.Claw;
 import org.firstinspires.ftc.teamcode.prototype.Elbow;
 import org.firstinspires.ftc.teamcode.prototype.Wrist;
@@ -18,13 +21,13 @@ import org.firstinspires.ftc.teamcode.roadrunner.tuning.TuningOpModes;
 @Autonomous(name="SpecimenRun2", group="Linear OpMode")
 public final class HighChamberSpecimens extends LinearOpMode {
 
+    Initialization initialization;
 
-
-    Elbow elbow;
     Slide slide;
-
+    TouchSensor slideLimit;
+    Elbow elbow;
+    TouchSensor elbowLimit;
     Wrist wrist;
-    //ProtoClawLeft clawLeft;
     Claw claw;
 
 
@@ -34,12 +37,19 @@ public final class HighChamberSpecimens extends LinearOpMode {
         slide = new Slide(this);
         elbow = new Elbow(this);
         wrist = new Wrist(this);
-        //clawLeft = new ProtoClawLeft(this);
         claw = new Claw(this);
 
-        Pose2d beginPose = new Pose2d(0, -55, Math.toRadians(90));
+        slideLimit = hardwareMap.get(TouchSensor.class, "slideLimit");
+        elbowLimit = hardwareMap.get(TouchSensor.class, "elbowLimit");
+
+
+        initialization = new Initialization(slide, slideLimit, elbow, elbowLimit);
+
+        Pose2d beginPose = new Pose2d(10, -55, Math.toRadians(90));
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+            initialization.initialization();
 
             waitForStart();
 
@@ -116,44 +126,83 @@ public final class HighChamberSpecimens extends LinearOpMode {
             //Mesloh attempt to use afterDisp to hang a specs.
             TrajectoryActionBuilder trajectoryActionBuilderMez2= drive.actionBuilder(beginPose)
 
+
                 .afterTime(0,claw.action(1)) //close claw
-                .afterTime(0, elbow.action( -1165, 0.5))
+                .afterTime(0, elbow.action(-1300, 0.5))
                 .afterTime(0, slide.action(-1100, 0.5))
                 .waitSeconds(1)
-                .splineToConstantHeading(new Vector2d( 0,-29), Math.toRadians(90)) //move to chamber
+                .splineToConstantHeading(new Vector2d( 10,-29), Math.toRadians(90)) //move to chamber
                 .afterTime(0, claw.action(0)) //begins when the action after it begins
-                .waitSeconds(.1)
-                .setTangent(Math.toRadians(360))
-                .splineToSplineHeading(new Pose2d(19, -30, Math.toRadians(180)), Math.toRadians(360))
-                .afterTime(0, elbow.action( 0, 0.5))
+                .afterTime(0, elbow.action(0, 0.5))
                 .afterTime(0, slide.action(0, 0.5))
-                //.waitSeconds(.1)
-                .splineToSplineHeading(new Pose2d(36, -30, Math.toRadians(270)), Math.toRadians(360))
-                .setTangent(Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(36, -12), Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d( 48, -10), Math.toRadians(90))
-                .afterTime(0, elbow.action( -300, .75))
-                .afterTime(0, slide.action( -300, 0.75))
                 .waitSeconds(.1)
-                .setTangent(Math.toRadians(270))
-                .splineToConstantHeading(new Vector2d(45, -50), Math.toRadians(270), new TranslationalVelConstraint(10)) //pickup from wall
-                .afterTime(0, claw.action(1))
-                .waitSeconds(.5)
-                .afterTime(0, elbow.action( -1165, 0.5))
-//                .afterTime(0, slide.action( -1100, 0.5))
-                .setTangent(Math.toRadians(45))
-                .afterTime(0, slide.action(-1100, 0.5))
-                .setTangent(Math.toRadians(45))
-
-
+                .setTangent(Math.toRadians(0))
+                .splineToSplineHeading(new Pose2d(19, -30, Math.toRadians(180)), Math.toRadians(0))  //head right while rotating
+////           .afterTime(0, protoSlideTheta.action( 0, 0.5))
+////           .afterTime(0, protoLinearSlide.action(0, 0.5))
+                .splineToSplineHeading(new Pose2d(30, -30, Math.toRadians(270)), Math.toRadians(360))//.setReversed(true)
+                .splineToConstantHeading(new Vector2d(36, -12), Math.toRadians(90))//.setReversed(true)
+                .splineToConstantHeading(new Vector2d(46, -12), Math.toRadians(0))//.setReversed(true)
+////           .afterTime(0, protoSlideTheta.action( -300, .75))
+////           .afterTime(0, protoLinearSlide.action( -300, 0.75))
+//                .splineToConstantHeading(new Vector2d(48, -40), Math.toRadians(270), new TranslationalVelConstraint(100))  //push sample
+//                .splineToConstantHeading(new Vector2d(48, -50), Math.toRadians(270), new TranslationalVelConstraint(10)) //slow down for pickup
+////           .afterTime(0, claw.action(1))
+////           .afterTime(0, protoSlideTheta.action( -1165, 0.5))
+////                .afterTime(0, protoLinearSlide.action( -1100, 0.5))
+//                .waitSeconds(.1)
+//                .setTangent(Math.toRadians(45))
 //                .setReversed(true)
-//                .splineToSplineHeading(new Pose2d(0, -51, Math.toRadians(180)), Math.toRadians(180))
-//                .splineToSplineHeading(new Pose2d(0,-29, Math.toRadians(180)), Math.toRadians(45)) //move to chamber
-                .strafeToLinearHeading(new Vector2d(0, -27), Math.toRadians(90.1))
-                .afterTime(0, claw.action(0))
-                .afterTime(0, elbow.action( 0, 0.5))
-                .afterTime(0, slide.action(0, 0.5))
-                .waitSeconds(1)
+//                .splineToSplineHeading(new Pose2d(10,-29, Math.toRadians(90)), Math.toRadians(90)) //move to chamber
+////           .afterTime(0, claw.action(0))
+////           .afterTime(0, protoSlideTheta.action( 0, 0.5))
+////           .afterTime(0, protoLinearSlide.action(0, 0.5))
+//                .waitSeconds(.1)
+
+
+
+
+
+
+
+//                .afterTime(0,claw.action(1)) //close claw
+//                .afterTime(0, elbow.action( -1165, 0.5))
+//                .afterTime(0, slide.action(-1100, 0.5))
+//                .waitSeconds(1)
+//                .splineToConstantHeading(new Vector2d( 0,-29), Math.toRadians(90)) //move to chamber
+//                .afterTime(0, claw.action(0)) //begins when the action after it begins
+//                .waitSeconds(.1)
+//                .setTangent(Math.toRadians(360))
+//                .splineToSplineHeading(new Pose2d(19, -30, Math.toRadians(180)), Math.toRadians(360))
+//                .afterTime(0, elbow.action( 0, 0.5))
+//                .afterTime(0, slide.action(0, 0.5))
+//                //.waitSeconds(.1)
+//                .splineToSplineHeading(new Pose2d(36, -30, Math.toRadians(270)), Math.toRadians(360))
+//                .setTangent(Math.toRadians(90))
+//                .splineToConstantHeading(new Vector2d(36, -12), Math.toRadians(90))
+//                .splineToConstantHeading(new Vector2d( 48, -10), Math.toRadians(90))
+//                .afterTime(0, elbow.action( -300, .75))
+//                .afterTime(0, slide.action( -300, 0.75))
+//                .waitSeconds(.1)
+//                .setTangent(Math.toRadians(270))
+//                .splineToConstantHeading(new Vector2d(45, -50), Math.toRadians(270), new TranslationalVelConstraint(10)) //pickup from wall
+//                .afterTime(0, claw.action(1))
+//                .waitSeconds(.5)
+//                .afterTime(0, elbow.action( -1165, 0.5))
+////                .afterTime(0, slide.action( -1100, 0.5))
+//                .setTangent(Math.toRadians(45))
+//                .afterTime(0, slide.action(-1100, 0.5))
+//                .setTangent(Math.toRadians(45))
+//
+//
+////                .setReversed(true)
+////                .splineToSplineHeading(new Pose2d(0, -51, Math.toRadians(180)), Math.toRadians(180))
+////                .splineToSplineHeading(new Pose2d(0,-29, Math.toRadians(180)), Math.toRadians(45)) //move to chamber
+//                .strafeToLinearHeading(new Vector2d(0, -27), Math.toRadians(90.1))
+//                .afterTime(0, claw.action(0))
+//                .afterTime(0, elbow.action( 0, 0.5))
+//                .afterTime(0, slide.action(0, 0.5))
+//                .waitSeconds(1)
                 ;
 
 
