828741c9431621dda0291ffc72090bf82df21ec2
InnovatorsRobotics <innovators3311@gmail.com>
2024-11-16 11:53:51 -0500

    Finished auto bucket

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBucket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBucket.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.TurnConstraints;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import org.firstinspires.ftc.teamcode.RobotChildren.Slide;
@@ -19,9 +21,6 @@ public class AutoBucket
     Elbow elbow;
     Wrist wrist;
     Claw claw;
-    private double target_X;
-    private double target_Y;
-    private double target_heading;
 
     public AutoBucket(MecanumDrive drive, Slide slide, Elbow elbow, Wrist wrist, Claw claw)
     {
@@ -34,17 +33,27 @@ public class AutoBucket
 
     public void bucketRun(double x, double y, double heading, int tag)
     {
+        double target_X;
+        double target_Y;
+        double targetWaypoint_X;
+        double targetWaypoint_Y;
+        double target_heading;
+
         if (tag == 16 || tag == 15 || tag == 14)
         {
-            target_X = -51;
-            target_Y = -56;
-            target_heading = 225;
+            target_X = -60;
+            target_Y = -60;
+            targetWaypoint_X = -45;
+            targetWaypoint_Y = -45;
+            target_heading = Math.toRadians(225);
         }
         else
         {
-            target_X = 51;
-            target_Y = 56;
-            target_heading = 45;
+            target_X = 60;
+            target_Y = 60;
+            targetWaypoint_X = 45;
+            targetWaypoint_Y = 45;
+            target_heading = Math.toRadians(45);
         }
 
         TrajectoryActionBuilder bucket = drive.actionBuilder(new Pose2d(x,y, heading))
@@ -52,15 +61,16 @@ public class AutoBucket
                 .afterTime(0, wrist.action(1))
                 .afterTime(0, elbow.action(-2500, 1))
                 .afterTime(0, slide.action(0, 0.5))
-                .strafeTo(new Vector2d(target_X + 5, target_Y + 5))
-                .turnTo(Math.toRadians(target_heading))
-                .strafeTo(new Vector2d(target_X,target_Y))
+//                .strafeTo(new Vector2d(targetWaypoint_X, targetWaypoint_Y),new TranslationalVelConstraint(100))
+//                .splineToConstantHeading(new Vector2d(targetWaypoint_X, targetWaypoint_Y), target_heading - 45, new TranslationalVelConstraint(100))
+                .splineToSplineHeading(new Pose2d(targetWaypoint_X, targetWaypoint_Y, target_heading), target_heading, new TranslationalVelConstraint(100))
+                .waitSeconds(0.25)
+                .splineToConstantHeading(new Vector2d(target_X,target_Y), target_heading)
                 .afterTime(0, slide.action(-2175, 1))
-                .waitSeconds(0.5)
                 .afterTime(0, elbow.action(-2400, 1))
                 .waitSeconds(0.5)
                 .afterTime(0, wrist.action(0))
-                .waitSeconds(0.5);
+                .waitSeconds(0.1);
 
 
         Action bucketAction = bucket.build();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotChildren/NessieTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotChildren/NessieTeleOp.java
@@ -72,11 +72,11 @@ public class NessieTeleOp extends LinearOpMode
         while (opModeIsActive())
         {
             // Drive Code
-//            centricDrive.drive(gamepad1.left_stick_x, gamepad1.left_stick_y, imu.getAngle(), gamepad1.right_trigger,
-//                    centricDrive.whichTurnMode(turnToHeading.turnToHeading(gamepad1.right_stick_x, gamepad1.right_stick_y, 0.2, 0.2),
-//                            gamepad1.right_stick_x, gamepad1.back, time.seconds())
-//            );
-            drive.gamepadController(gamepad1);
+            centricDrive.drive(gamepad1.left_stick_x, gamepad1.left_stick_y, imu.getAngle(), gamepad1.right_trigger,
+                    centricDrive.whichTurnMode(turnToHeading.turnToHeading(gamepad1.right_stick_x, gamepad1.right_stick_y, 0.2, 0.2),
+                            gamepad1.right_stick_x, gamepad1.back, time.seconds())
+            );
+            //drive.gamepadController(gamepad1);
 
 
             // Algorithms
